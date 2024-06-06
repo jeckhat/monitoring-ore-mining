b3dbf540d528edab53a2632394fe5d47347c1053
Zaki Mudzakir H <zaki.hidayatullah2@bankmandiri.co.id>
2024-06-06 17:54:45 +0700

    feat: add start and stop miner

---
+++ miner.js
@@ -0,0 +1,44 @@
+const { exec } = require('child_process');
+
+const args2 = process.argv[1]; // stop or start
+const args3 = process.argv[2];
+
+exec(`pm2 ${args2} ${args3}`, (error, _, stderr) => {
+    if (error) {
+        console.log(JSON.stringify({
+            status: false,
+            message: error.message,
+            data: []
+        }))
+        return;
+    }
+    if (stderr) {
+        console.log(JSON.stringify({
+            status: false,
+            message: stderr,
+            data: []
+        }))
+        return;
+    }
+
+    exec('node status.js', (error, stdout, stderr) => {
+        if (error) {
+            console.log(JSON.stringify({
+                status: false,
+                message: error.message,
+                data: []
+            }))
+            return;
+        }
+        if (stderr) {
+            console.log(JSON.stringify({
+                status: false,
+                message: stderr,
+                data: []
+            }))
+            return;
+        }
+
+        console.log(stdout.split("\n").join(""))
+    });
+});
